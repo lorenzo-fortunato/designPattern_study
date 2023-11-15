4a080e9ca0df851e20f320edb4d1145c93972747
Lorenzo Fortunato <lorenzo.fortunato@alten.it>
2023-11-15 13:05:49 +0100

    rename + gitignore_ok

--- esercizioAbstractFactory2/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const abstractFactory_1 = require("./factory/abstractFactory");
-const main = (factoryType) => {
-    const abstractFactory = (0, abstractFactory_1.createFactory)(factoryType);
-    console.log(abstractFactory.createButton().click());
-};
-main("Pc");
--- esercizioProxy3/realInternet.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RealInternet = void 0;
-class RealInternet {
-    connect(website) {
-        console.log("Connecting to website: " + website);
-    }
-}
-exports.RealInternet = RealInternet;
